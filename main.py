9d6568b8dcff67c7b9df70a86caa89750f3acf1e
unknown <juan.gutierrez.ttc.840@unilibre.edu.co>
2026-09-05 21:35:53 -0500

    Persistencia de cuenta

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    context = p.chromium.launch_persistent_context(
+        user_data_dir="whatsapp_session",
+        headless=False
+    )
 
-    page = browser.new_page()
+    page = context.pages[0] if context.pages else context.new_page()
 
     page.goto("https://web.whatsapp.com")
 
     print("WhatsApp Web abierto.")
-    print("Escanea el código QR si es necesario.")
+    input("Cuando estés dentro de WhatsApp, presiona ENTER...")
 
-    input("Presiona ENTER cuando WhatsApp esté listo...")
-
-    print("Sesión lista.")
+    print("URL:", page.url)
+    print("Título:", page.title())
 
     input("Presiona ENTER para cerrar...")
 
-    browser.close()
+    context.close()
